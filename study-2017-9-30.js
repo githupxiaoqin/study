499f9fe01eb338f293626382bea93cf57ea178da
githubxiaoqin <1324641287@qq.com>
2017-10-31 22:30:29 +0800

    2017-10-31

---
+++ study-2017-9-30.js
@@ -0,0 +1,21 @@
+
+//创建数组
+// var arry
+// var arry = 2
+// var arry = 3
+// var arry = 4
+
+
+// arry[0] = 1
+// arry[1] = 2
+// arry[2] = 3
+
+let arry = new arryt(5)
+arry[0] = 1
+arry[1] = 2
+arry[2] = 3
+// let arry = [2,10]
+console.log( arry)
+
+// let averangeTem = [2,10]
+// console.log(averangeTem)
